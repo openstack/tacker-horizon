Tesora/tesora-horizon

Support for resizing a trove instance
Added menu item and dialog to allow the
user to select a new flavor for their
trove instance

Change-Id: I5ef42067887e1f1f1f5ee3224df7b6391be0a375
Implements: blueprint trove-resize-instance-dialog

openstack_dashboard/api/trove.py
@@ -82,6 +82,11 @@ def instance_resize_volume(request, instance_id, size):
     return troveclient(request).instances.resize_volume(instance_id, size)
 
 
+def instance_resize_instance(request, instance_id, flavor_id):
+    return troveclient(request).instances.resize_instance(instance_id,
+                                                          flavor_id)
+
+
 def instance_backups(request, instance_id):
     return troveclient(request).instances.backups(instance_id)
 

openstack/tacker-horizon

trove.py
@@ -80,6 +80,11 @@ def instance_resize_volume(request, instance_id, size):
     return troveclient(request).instances.resize_volume(instance_id, size)
 
 
+def instance_resize(request, instance_id, flavor_id):
+    return troveclient(request).instances.resize_instance(instance_id,
+                                                          flavor_id)
+
+
 def instance_backups(request, instance_id):
     return troveclient(request).instances.backups(instance_id)
 