da76c441d751b3b4a915d7583861817f053bddaf
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2015-07-18 12:40:19 -0500

    Drop down list for selecting VNFD IDs added
    The existing implementation requires an end user to input the VNFD ID in
    a text box.This commit provides the ability for an user to select the
    VNFD IDs from a drop down list box.
    
    Change-Id: I3f6203ff206638fade05834a8ba876a3b0a2fd8a
    Closes-bug: #1475901

--- tacker_horizon/openstack_dashboard/dashboards/nfv/vnfmanager/forms.py
+++ tacker_horizon/openstack_dashboard/dashboards/nfv/vnfmanager/forms.py
@@ -12,6 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import logging
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -20,26 +21,36 @@ from horizon import forms
 from horizon import messages
 
 from tacker_horizon.openstack_dashboard import api
-from tacker_horizon.openstack_dashboard.dashboards.nfv.vnfmanager.tables import VNFManagerItem
-from tacker_horizon.openstack_dashboard.dashboards.nfv.vnfmanager.tables import VNFManagerItemList
 
+LOG = logging.getLogger(__name__)
 
 class AddService(forms.SelfHandlingForm):
     vnf_name = forms.CharField(max_length=80, label=_("VNF Name"))
-    vnfd_id = forms.CharField(max_length=80, label=_("VNF Catalog ID"))
+    vnfd_id = forms.ChoiceField(label=_("VNF Catalog Name"))
+
+    def __init__(self, request, *args, **kwargs):
+        super(AddService, self).__init__(request, *args, **kwargs)
+
+        try:
+            vnfd_list = api.tacker.vnfd_list(request)
+            available_choices = [(vnf['id'],vnf['name'])
+                                 for vnf in vnfd_list]
+        except Exception as e:
+            msg = _('Failed to retrieve available VNF Catalog names: %s') % e
+            LOG.error(msg)
+
+        self.fields['vnfd_id'].choices = [('', _('Select a VNF Catalog Name'))
+                                          ]+available_choices
 
     def handle(self, request, data):
         try:
             vnf_name = data['vnf_name']
             vnfd_id = data['vnfd_id']
-            print "VNF Name: " + vnf_name
-            print "VNFD Id: " + vnfd_id
             vnf_arg = {'vnf': {'vnfd_id': vnfd_id, 'name':  vnf_name}}
-            vnf_instance = api.tacker.create_vnf(request, vnf_arg)
-            print "VNF Instance: " + str(vnf_instance)
+            api.tacker.create_vnf(request, vnf_arg)
             messages.success(request,
                              _('VNF %s has been created.') % vnf_name)
             return True
         except Exception:
             exceptions.handle(request,
-                              _('Unable to create service.'))
+                              _('Unable to create VNF.'))
